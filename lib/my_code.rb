1e1146a033545103e52b79ee3b7e4e6d889f65dd
Justin Maple <justinamaple@gmail.com>
2019-08-19 07:42:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,6 +15,6 @@ def map_to_square(source_array)
   source_array.map { |num| num**2 }  
 end
 
-def reduce_to_total(source_array, start = 0)
+def reduce_to_total(source_array)
   source_array.reduce(start) { |sum, n| sum + n }
 end
