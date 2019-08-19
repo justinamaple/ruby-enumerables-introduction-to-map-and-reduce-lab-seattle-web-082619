96fe833b6fdd0741ce0f5646ed0a272234ebafc7
Justin Maple <justinamaple@gmail.com>
2019-08-19 07:43:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,3 +18,7 @@ end
 def reduce_to_total(source_array, starting_point = 0)
   source_array.reduce(starting_point) { |sum, n| sum + n }
 end
+
+def reduce_to_all_true(source_array)
+  source_array.reduce { |
+end
